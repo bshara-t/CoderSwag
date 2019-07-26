0be0e4fa31d63b9c7a23bcd7c598a07877969dfe
bshara-t <bshara2704@gmail.com>
2019-07-26 16:42:01 +0300

    lesso-08

--- app/src/main/java/com/repoai/bshara/coderswag/controler/ProductsActivity.kt
+++ app/src/main/java/com/repoai/bshara/coderswag/controler/ProductsActivity.kt
@@ -1,17 +1,39 @@
 package com.repoai.bshara.coderswag.controler
 
+import android.content.res.Configuration
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.GridLayoutManager
+import com.repoai.bshara.coderswag.Adapters.ProductsAdapter
 import com.repoai.bshara.coderswag.R
 import com.repoai.bshara.coderswag.Utilities.EXTRA_CATEGORY
+import com.repoai.bshara.coderswag.oneServices.DataService
+import kotlinx.android.synthetic.main.activity_products.*
 
 class ProductsActivity : AppCompatActivity() {
+
+    lateinit var adapter: ProductsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_products)
 
         val categoryType = intent.getStringExtra(EXTRA_CATEGORY)
-        println(categoryType)
+        adapter = ProductsAdapter(this, DataService.getProducts(categoryType))
+
+        var spanCount = 2
+        val orientation = resources.configuration.orientation
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            spanCount = 3
+        }
+
+        val screenSize = resources.configuration.smallestScreenWidthDp
+        if (screenSize > 720) {
+            spanCount = 3
+        }
+
+        val layoutManager = GridLayoutManager(this, spanCount)
+        producrLstView.layoutManager = layoutManager
+        producrLstView.adapter = adapter
     }
 }
